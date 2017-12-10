577bfd4639d0bc40cf6409d0604a7744c5c4dc2d
Tony Hirst <tony.hirst@gmail.com>
2017-12-10 16:50:51 +0000

    Install folium_maps magic from github

--- magics/folium_magic/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Tikz magic"""
-__version__ = '0.0.1'
-
-from .folium_magic import FoliumMagic
-
-def load_ipython_extension(ipython):
-    ipython.register_magics(FoliumMagic)
--- magics/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='patchedMagics_maps',
-	install_requires=['folium','fiona'],
-      packages=['folium_magic']
-)
