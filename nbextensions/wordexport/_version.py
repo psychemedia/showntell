db7fdb4331d2532c6bdfba31567dff88eb736420
Tony Hirst <tony.hirst@gmail.com>
2018-01-13 20:56:50 +0000

    Physics repo - first commit to init installation

--- nbextensions/wordexport/_version.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) The Open University, 2017
-# Distributed under the terms of the Modified BSD License.
-
-version_info = (0, 0, 2, 'dev')
-__version__ = '.'.join(map(str, version_info))
